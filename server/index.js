8553eaa44efd3e5f7c64dde1988a585dcb2fa510
James Issac <jamesissac@Jamess-MacBook-Pro.local>
2026-02-12 06:25:14 -0800

    safe

--- server/index.js
+++ server/index.js
@@ -2,5 +2,11 @@ const express = require("express");
 const mongodb = require("mongodb");
 const dotenv = require("dotenv").config();
 
-
+const App = express(); 
 const PORT = 3000;
+
+
+
+App.listen(PORT, ()=> {
+    console.log(`Server Running @PORT: ${PORT}`);
+})
---
+++ server/model/AdminModel.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const {Schema} = express;
+
+//const AdminSchema = async (()=> {})
